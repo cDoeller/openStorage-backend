59e3a3ba9e52c6ffc697b51dd4f061e91c0eb2d6
Christian Doeller <mail@christiandoeller.de>
2024-04-08 10:19:36 +0200

    added no password sending in user routes + framework for pwd reset

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -9,7 +9,7 @@ const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 router.get("/artists", (req, res) => {
   User.find({ isArtist: true })
     .select("user_name _id artworks")
-    //.populate("artworks")
+    .populate("artworks")
     .then((allArtists) => {
       res.json(allArtists);
     })
@@ -23,7 +23,8 @@ router.get("/artists", (req, res) => {
 // test 66114ddfc49cfc974744bc0e
 router.get("/:_id", isAuthenticated, (req, res) => {
   User.findById(req.params._id)
-    // .populate("artworks")
+    .select("-password")
+    .populate("artworks")
     .then((oneUserData) => {
       res.json(oneUserData);
     })
@@ -33,6 +34,8 @@ router.get("/:_id", isAuthenticated, (req, res) => {
 // UPDATE one user
 router.put("/:_id/update", isAuthenticated, (req, res) => {
   User.findByIdAndUpdate(req.params._id, req.body, { new: true })
+    .select("-password")
+    .populate("artworks")
     .then((updatedUser) => {
       res.json(updatedUser);
     })
@@ -53,5 +56,14 @@ router.delete("/:_id/delete", isAuthenticated, (req, res) => {
       res.json(err);
     });
 });
+
+// UPDATE password-reset
+router.put ("/:_id/pwd", isAuthenticated, (req,res)=>{
+  // check pwd regex / empty
+  // make salt
+  // hash pw
+  // update user pw in db
+  // send back name, email, id
+})
 
 module.exports = router;
