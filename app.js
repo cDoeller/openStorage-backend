e77f61b83f256f1d53864dfa1eadddbda6d546b1
Nadine Rangosch <nrangosch@gmail.com>
2024-04-09 20:15:44 +0200

    basic routes for city added

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.use("/api/user", userRoutes);
 const artworkRoutes = require("./routes/artwork.routes")
 app.use("/api/artworks", artworkRoutes)
 
+const cityRoutes = require("./routes/city.routes")
+app.use("/api/city", cityRoutes)
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
